6f0c687455b5aaae976af6009d9150686bd03f70
Mohit Sarkar <mohitsarkar256@gmail.com>
2024-03-11 15:19:03 +0530

    add new filr

--- Areaof_Circle.java
+++ Areaof_Circle.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Areaof_Circle {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner scan= new Scanner(System.in);
+        int r= scan.nextInt();
+        System.out.println(3.14*r*r);
+
     }
 }
